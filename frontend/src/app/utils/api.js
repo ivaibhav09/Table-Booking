f36ace65c94473797a78e241e66c5b807a19830d
Vaibhav Mishra <118072316+ivaibhav09@users.noreply.github.com>
2025-01-03 17:25:45 +0530

    Update api.js

--- frontend/src/app/utils/api.js
+++ frontend/src/app/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1/table/',
+  baseURL: https://table-booking-api.vercel.app/ || 'http://localhost:8000/api/v1/table/',
   headers: {
     'Content-Type': 'application/json',
   },
